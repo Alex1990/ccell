603dfdfe8e45e31b95128a533f354236e20e39d8
Alex Chao <alexchao1990@gmail.com>
2015-07-07 16:55:16 +0800

    Swap two strings

---
+++ swap_str_ptr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+void
+swap_str_ptr(char **str_a, char **str_b);
+
+int
+main()
+{
+  char *s1 = "hello";
+  char *s2 = ", world!";
+
+  printf("Before swap:\n");
+  printf("============\n");
+  printf("s1: %s\n", s1);
+  printf("s2: %s\n", s2);
+
+  // swap
+  swap_str_ptr(&s1, &s2);
+
+  printf("\nAfter swap:\n");
+  printf("============\n");
+  printf("s1: %s\n", s1);
+  printf("s2: %s\n", s2);
+
+  return 0;
+}
+
+void
+swap_str_ptr(char **str_a, char **str_b)
+{
+  char *temp = *str_a;
+  *str_a = *str_b;
+  *str_b = temp;
+}
